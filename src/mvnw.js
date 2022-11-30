e36ecc1d013920d8a666c072d885413f5be78639
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2022-11-30 18:50:41 +0300

    feat(#93): ignore status for informative commands

--- src/mvnw.js
+++ src/mvnw.js
@@ -63,7 +63,10 @@ module.exports = function(args) {
             shell: shell(),
           }
         );
-      status.start(args[0], params.find(element => element.includes("targetDir")).split("=")[1]);
+      let target = params.find(element => element.includes("targetDir"));
+      if(target != undefined){
+        status.start(args[0], target.split("=")[1]);
+      }
       result.on('close', code => {
         if (code !== 0) {
           throw new Error('The command "' + cmd + '" exited with #' + code + ' code');
--- src/status.js
+++ src/status.js
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
