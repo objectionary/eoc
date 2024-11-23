32e0759f316bfd432b4c9d4e5a89a890238260a0
ostapenkovitalij <vitost666@gmail.com>
2024-11-23 19:30:39 +0300

    fix(#368): fix up todo needful time

--- src/commands/java/compile.js
+++ src/commands/java/compile.js
@@ -34,7 +34,7 @@ const path = require('path');
 module.exports = function(opts) {
   const target = path.resolve(opts.target);
   /**
-   * @todo #368 Wrap logs in 'elapsed'
+   * @todo #368:30min Wrap logs in 'elapsed'
    * - It is necessary to use 'elapsed' in all logging cases that require output of elapsed time
    */
   return mvnw(['compiler:compile'].concat(flags(opts)), opts.target, opts.batch).then((r) => {
