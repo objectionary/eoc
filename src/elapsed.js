b37f5bf15ce7bcd7efbdcaaf8c624f1cd7a1e57f
ostapenkovitalij <vitost666@gmail.com>
2024-11-26 21:37:08 +0300

    fix(#368): formatted

--- src/elapsed.js
+++ src/elapsed.js
@@ -23,7 +23,8 @@
  */
 
 /**
- * @todo #368:30min Check if `elapsed` and its tests are in the right place
+ * @todo #368:30min Decide if the `elapsed` utility function is in the right place,
+ * consider relocating it and its test file if needed
  */
 
 /**
