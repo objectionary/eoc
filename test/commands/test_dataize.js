c85c9cb3a13563a07c08e6d48a4134693e9cf2e1
Yegor Bugayenko <yegor256@gmail.com>
2024-12-07 09:25:18 +0300

    #408: tests skipped

--- test/commands/test_dataize.js
+++ test/commands/test_dataize.js
@@ -30,6 +30,7 @@ const {runSync, parserVersion, homeTag, weAreOnline} = require('../helpers');
 const options = [
   {lang: 'Java', version: parserVersion, tag: homeTag},
   {lang: 'Java', version: '0.45.0', tag: '0.45.0'},
+  {lang: 'Java', version: '0.44.0', tag: '0.44.0'},
   {lang: 'Java', version: '0.41.3', tag: '0.41.3'},
   {lang: 'Java', version: '0.40.3', tag: '56b2f37d32d71f06e2d298325164485972197a06'},
   // {lang: 'JavaScript', version: parserVersion, tag: homeTag},
