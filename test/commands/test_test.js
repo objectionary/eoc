4f44b2e24e9807f8309057d5127eca221eabc37e
Yegor Bugayenko <yegor256@gmail.com>
2024-12-16 17:26:28 +0300

    #429: up

--- test/commands/test_test.js
+++ test/commands/test_test.js
@@ -60,8 +60,8 @@ describe('test', function() {
       '--easy',
       `--parser=${parser}`,
       `--home-tag=${hash}`,
-      '--stack=16M',
-      '--heap=128M',
+      '--stack=64M',
+      '--heap=1G',
       '-s', path.resolve(home, 'src'),
       '-t', path.resolve(home, 'target'),
       `--language=${lang}`
