a9a37eccd079ec30c95d2670bf45ba2df56c7b40
rultor <me@rultor.com>
2025-03-28 02:07:26 +0000

    set version to 0.29.0

--- src/version.js
+++ src/version.js
@@ -26,6 +26,6 @@
 // The values here are replaced automatically by the .rultor.yml script,
 // at the "release" pipeline:
 module.exports = {
-  what: '0.0.0',
-  when: '0000-00-00'
+  what: '0.29.0',
+  when: '2025-03-28'
 };
