a22a974c7d3393d33f86ae5d9577ef671040a912
Ernest <matskevich5555@gmail.com>
2025-05-22 03:05:28 +0300

    add license in package-info.java

--- mvnw/src/main/java/org/eolang/package-info.java
+++ mvnw/src/main/java/org/eolang/package-info.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+
 /**
  * EO inspection utilities.
  * Includes HTTP server for testing (under developing).
