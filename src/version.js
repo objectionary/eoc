17510dfe1cd1b6fd0f444fc2471a0643d249c03e
Rultor.com <gpg@rultor.com>
2025-08-21 15:47:02 +0000

    set version to 0.32.0

--- src/version.js
+++ src/version.js
@@ -6,6 +6,6 @@
 // The values here are replaced automatically by the .rultor.yml script,
 // at the "release" pipeline:
 module.exports = {
-  what: '0.0.0',
-  when: '0000-00-00'
+  what: '0.32.0',
+  when: '2025-08-21'
 };
