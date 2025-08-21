22009b26742a28915b4a596b0de5daec779984a5
Vladislav Yevtushenko <vladislav_yevtushenko@epam.com>
2025-08-21 11:00:58 +0500

    fix(homebrew): add homebrew formula
    it addresses this issue https://github.com/objectionary/eoc/issues/449

---
+++ Formula/eolang.rb
@@ -0,0 +1,20 @@
+require "language/node"
+class Eolang < Formula
+  desc "Command-line Tool-Kit"
+  homepage "https://github.com/objectionary/eoc"
+  url "https://github.com/objectionary/eoc/archive/refs/tags/0.31.0.tar.gz"
+  version "0.31.0"
+  sha256 "d9238aae0d3911a3bc13c35a9351ba600ee4952f9cc36227242b5fa113ea4ebf"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/eoc", "--version"
+  end
+end
