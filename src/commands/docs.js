2b7debff9ddc30a684fcdc6f50346a43f5dc12a4
Бенца Даниил Игоревич <dibentsa@edu.hse.ru>
2025-09-16 20:34:11 +0300

    Moved XSL code to separate file

--- src/commands/docs.js
+++ src/commands/docs.js
@@ -49,75 +49,21 @@ function transformDocument(xmir, xsl) {
       } 
     }
   );
-
   return html;
 }
 
-const BLOCK_XSL = `<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-  <xsl:template match="/">
-    <div class="app-block">
-      <xsl:for-each select="//o[(@name and @name != 'φ') and (not(@base) or (@base != '∅' and @base != 'ξ'))]">
-        <xsl:if test="//comments/comment[@line = current()/@line]">
-          <div class="object-block">
-            <xsl:variable name="fullname">
-              <xsl:for-each select="ancestor-or-self::o[@name and @name != 'φ']">
-                <xsl:value-of select="@name"/>
-                <xsl:choose>
-                  <xsl:when test="position() != last()">.</xsl:when>
-                </xsl:choose>
-              </xsl:for-each>
-            </xsl:variable>
-            <h2 class="object-title"><xsl:value-of select="$fullname"/></h2>
-            <p class="object-sign">
-              <xsl:value-of select="$fullname"/>(<xsl:for-each select="current()/o[@base and @base = '∅']">
-                <xsl:value-of select="@name"/>
-                <xsl:choose>
-                  <xsl:when test="position() != last()">, </xsl:when>
-                </xsl:choose>
-              </xsl:for-each>)</p>
-            <p class="object-desc">
-              <xsl:call-template name="break">
-                <xsl:with-param name="text" select="//comments/comment[@line = current()/@line]"/>
-              </xsl:call-template>
-            </p>
-          </div>
-        </xsl:if>
-      </xsl:for-each>
-    </div>
-  </xsl:template>
-
-  <xsl:template name="break">
-    <xsl:param name="text" select="string(.)"/>
-    <xsl:choose>
-      <xsl:when test="contains($text, '\\n')">
-        <xsl:value-of select="substring-before($text, '\\n')"/>
-        <br/>
-        <xsl:call-template name="break">
-          <xsl:with-param 
-            name="text" 
-            select="substring-after($text, '\\n')"
-          />
-        </xsl:call-template>
-      </xsl:when>
-      <xsl:otherwise>
-        <xsl:value-of select="$text"/>
-      </xsl:otherwise>
-    </xsl:choose>
-  </xsl:template>
-</xsl:stylesheet>`;
-
 /**
  * Creates documentation block from given XMIR
- * @param {String} path - path of XMIR
+ * @param {String} xmir_path - path of XMIR
  * @return {String} HTML block
  */
-function createXmirHtmlBlock(path) {
+function createXmirHtmlBlock(xmir_path) {
   try {
-    const xmir = fs.readFileSync(path).toString();
-    return transformDocument(xmir, BLOCK_XSL);
+    const xmir = fs.readFileSync(xmir_path).toString();
+    const xsl = fs.readFileSync(path.join(__dirname, '..', 'resources', 'xmir-transformer.xsl')).toString();
+    return transformDocument(xmir, xsl);
   } catch(error) {
-    throw new Error('Error while applying XSL to XMIR:', error);
+    throw new Error(`Error while applying XSL to XMIR: ${error.message}`);
   }
 }
 
@@ -171,46 +117,36 @@ module.exports = async function(opts) {
     const input = path.resolve(opts.target, '1-parse');
     const output = path.resolve(opts.target, 'docs');
     fs.mkdirSync(output, {recursive: true});
-
     const css = path.join(output, 'styles.css');
     fs.writeFileSync(css, '');
-
     const packages_info = {};
     const all_xmir_htmls = [];
-
     const xmirs = readXmirsRecursively(input);
     for (const xmir of xmirs) {
       const relative = path.relative(input, xmir);
       const name = path.parse(xmir).name;
-
       const xmir_html = createXmirHtmlBlock(xmir);
       const html_app = path.join(output, path.dirname(relative),`${name}.html`);
       fs.mkdirSync(path.dirname(html_app), {recursive: true});
       fs.writeFileSync(html_app, wrapHtml(xmir_html, css));
-
       const packages = path.dirname(relative).split(path.sep).join('.');
       const html_package = path.join(output, `package_${packages}.html`);
-
       if (!(packages in packages_info)) {
         packages_info[packages] = {
           xmir_htmls : [],
           path: html_package
         };
       }
-
       packages_info[packages].xmir_htmls.push(xmir_html);
       all_xmir_htmls.push(xmir_html);
     }
-
     for (const package_name of Object.keys(packages_info)) {
       fs.mkdirSync(path.dirname(packages_info[package_name].path), {recursive: true});
       fs.writeFileSync(packages_info[package_name].path,
         generatePackageHtml(package_name, packages_info[package_name].xmir_htmls, css));
     }
-
     const packages = path.join(output, 'packages.html');
     fs.writeFileSync(packages, generatePackageHtml('', all_xmir_htmls, css));
-
     console.info('Documentation generation completed in the %s directory', output);
   } catch (error) {
     console.error('Error generating documentation:', error);
---
+++ src/resources/xmir-transformer.xsl
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="/">
+    <div class="app-block">
+      <xsl:for-each select="//o[(@name and @name != 'φ') and (not(@base) or (@base != '∅' and @base != 'ξ'))]">
+        <xsl:if test="//comments/comment[@line = current()/@line]">
+          <div class="object-block">
+            <xsl:variable name="fullname">
+              <xsl:for-each select="ancestor-or-self::o[@name and @name != 'φ']">
+                <xsl:value-of select="@name"/>
+                <xsl:choose>
+                  <xsl:when test="position() != last()">.</xsl:when>
+                </xsl:choose>
+              </xsl:for-each>
+            </xsl:variable>
+            <h2 class="object-title"><xsl:value-of select="$fullname"/></h2>
+            <p class="object-sign">
+              <xsl:value-of select="$fullname"/>(<xsl:for-each select="current()/o[@base and @base = '∅']">
+                <xsl:value-of select="@name"/>
+                <xsl:choose>
+                  <xsl:when test="position() != last()">, </xsl:when>
+                </xsl:choose>
+              </xsl:for-each>)</p>
+            <p class="object-desc">
+              <xsl:call-template name="break">
+                <xsl:with-param name="text" select="//comments/comment[@line = current()/@line]"/>
+              </xsl:call-template>
+            </p>
+          </div>
+        </xsl:if>
+      </xsl:for-each>
+    </div>
+  </xsl:template>
+
+  <xsl:template name="break">
+    <xsl:param name="text" select="string(.)"/>
+    <xsl:choose>
+      <xsl:when test="contains($text, '\n')">
+        <xsl:value-of select="substring-before($text, '\n')"/>
+        <br/>
+        <xsl:call-template name="break">
+          <xsl:with-param 
+            name="text" 
+            select="substring-after($text, '\n')"
+          />
+        </xsl:call-template>
+      </xsl:when>
+      <xsl:otherwise>
+        <xsl:value-of select="$text"/>
+      </xsl:otherwise>
+    </xsl:choose>
+  </xsl:template>
+</xsl:stylesheet>
