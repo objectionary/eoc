7a308c747f60cfcd3a8b9015acea3e6ba1d918ce
Бенца Даниил Игоревич <dibentsa@edu.hse.ru>
2025-10-07 02:43:11 +0300

    added simple test for creating expected files

--- test/commands/test_docs.js
+++ test/commands/test_docs.js
@@ -9,11 +9,47 @@ const path = require('path');
 const {runSync} = require('../helpers');
 
 describe('docs', () => {
+  const home = path.resolve('temp/test-docs');
+  const target = path.resolve(home, 'target');
+  const source = path.resolve(home, 'src');
+  beforeEach(() => {
+    fs.rmSync(home, {recursive: true, force: true});
+    fs.mkdirSync(source, {recursive: true});
+  });
   /**
-   * Empty test
+   * Tests that the 'docs' command generates HTML files in the docs directory.
    * @param {Mocha.Done} done - Mocha callback signaling asynchronous completion
    */
-  it('empty test', (done) => {
+  it('generates HTML files for files and packages', (done) => {
+    const sample = path.join(source, 'foo', 'bar');
+    fs.mkdirSync(sample, {recursive: true});
+    const xmir1 = path.join(sample, 'test1.xmir');
+    fs.writeFileSync(xmir1, '<program name="test" />');
+    const xmir2 = path.join(sample, 'test1.xmir');
+    fs.writeFileSync(xmir2, '<program name="test" />');
+    runSync([
+      'docs',
+      '--verbose',
+      '-s', source,
+      '-t', target,
+    ]);
+    const docs = path.join(target, 'docs');
+    assert(fs.existsSync(docs), 'Expected the docs directory to be created but it is missing');
+
+    const test1_html = path.join(docs, 'foo/bar/test1.html');
+    assert(fs.existsSync(test1_html), `Expected file ${test1_html} but it was not created`);
+
+    const test2_html = path.join(docs, 'foo/bar/test2.html');
+    assert(fs.existsSync(test2_html), `Expected file ${test2_html} but it was not created`);
+
+    const package_foo_bar_html = path.join(docs, 'package_foo.bar.html');
+    assert(fs.existsSync(package_foo_bar_html), `Expected file ${package_foo_bar_html} but it was not created`);
+
+    const packages_html = path.join(docs, 'packages.html');
+    assert(fs.existsSync(packages), `Expected file ${packages} but it was not created`);
+
+    const css = path.join(docs, 'styles.css');
+    assert(fs.existsSync(css), `Expected file ${css} but it was not created`);
     done();
   });
 });
