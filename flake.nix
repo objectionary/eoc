be22d023ac203e1c293d559a45636592e36deaf9
devil <europedreadlydevil@gmail.com>
2025-10-20 12:56:28 +0300

    bug(https://github.com/objectionary/eoc/issues/741): Package eolang as a Nix derivation
    This commit packages eolang as a Nix derivation, enabling reproducible builds and easy installation. The derivation includes build and install phases to manage dependencies and create the executable. The old node2nix files are no longer needed.

--- flake.nix
+++ flake.nix
@@ -6,23 +6,45 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        node2nixPkgs = import ./nix/default.nix { inherit pkgs; };
-        eocPackage = node2nixPkgs.package;
+        eoc = pkgs.stdenv.mkDerivation {
+          pname = "eolang";
+          version = "0.0.0";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.nodejs_24 pkgs.makeWrapper ];
+
+          buildPhase = ''
+            mkdir -p $out/lib/node_modules/eolang
+            cp -r ./* $out/lib/node_modules/eolang
+            cd $out/lib/node_modules/eolang
+            npm install --omit=dev
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            ${pkgs.makeWrapper}/bin/makeWrapper \
+              $out/lib/node_modules/eolang/src/eoc.js \
+              $out/bin/eoc \
+              --set NODE_PATH $out/lib/node_modules
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Command-line toolkit for parsing, compiling, transpiling, optimizing, linking, dataizing, and running EOLANG programs";
+            license = licenses.mit;
+            maintainers = with maintainers; [ yourGitHubNick ];
+          };
+        };
       in
       {
-        packages.eoc = eocPackage;
-
-        apps.eoc = {
-          type = "app";
-          program = "${eocPackage}/bin/eoc";
+        packages = {
+          eoc = eoc;
         };
-
-        defaultPackage = eocPackage;
-        defaultApp = self.apps.eoc;
+        defaultPackage = eoc;
       }
     );
 }
