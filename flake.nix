345d6d4e4c072d2a2e170135ba3a6cf1414adf28
devil <europedreadlydevil@gmail.com>
2025-10-20 11:27:27 +0300

    bug(https://github.com/objectionary/eoc/issues/741): Use node2nix to manage node dependencies
    This commit introduces node2nix to manage node dependencies, simplifying
    the build process and improving reproducibility. It replaces the manual
    npm commands with a more declarative approach.

--- flake.nix
+++ flake.nix
@@ -3,52 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          eoc = pkgs.stdenv.mkDerivation {
-            pname = "eoc";
-            version = "latest";
-            src = ./.; 
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        node2nixPkgs = pkgs.callPackage ./nix/default.nix { };
+        eocPackage = node2nixPkgs.package;
+      in
+      {
+        packages.eoc = eocPackage;
 
-            nativeBuildInputs = [ pkgs.nodejs ];
+        # опционально — приложение (bin)
+        apps.eoc = {
+          type = "app";
+          program = "${eocPackage}/bin/eoc";
+        };
 
-            buildPhase = ''
-              npm install
-            '';
-
-            installPhase = ''
-              mkdir -p $out/bin
-              npm install -g . --prefix $out
-            '';
-
-            meta = with pkgs.lib; {
-              description = "Command-line toolkit for parsing, compiling, transpiling, optimizing, linking, dataizing, and running EOLANG programs";
-              homepage = "https://github.com/objectionary/eoc";
-              license = licenses.mit;
-              platforms = platforms.all;
-            };
-          };
-        in {
-          default = eoc;
-        }
-      );
-
-      apps = forAllSystems (system: {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/eoc";
-      });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.default);
-      defaultApp = forAllSystems (system: self.apps.${system});
-    };
+        defaultPackage = eocPackage;
+        defaultApp = self.apps.eoc;
+      }
+    );
 }
 
