98b8004d4a79c25dbba91305bb03e4f5be5b8129
devil <europedreadlydevil@gmail.com>
2025-10-22 13:12:57 +0300

    bug(https://github.com/objectionary/eoc/issues/741): Add SPDX headers to flake.nix
    This commit adds SPDX headers to the flake.nix file for license
    compliance and clarity.

--- flake.nix
+++ flake.nix
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2025 Objectionary.com
+# SPDX-License-Identifier: MIT
 {
   description = "Command-line toolkit for parsing, compiling, transpiling, optimizing, linking, dataizing, and running EOLANG programs";
 
