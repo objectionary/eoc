241f7ac3c26c2525faa22cbf9ebf6dc36bbf7879
Valentine <thayornswordsman@gmail.com>
2025-11-14 18:43:09 +0300

    Fix foreign.js command implementation

--- src/commands/foreign.js
+++ src/commands/foreign.js
@@ -12,10 +12,16 @@ const fs = require('fs');
  * @param {Hash} opts - All options
  */
 module.exports = function(opts) {
-  const file = path.resolve(opts.target, 'eo-foreign.json'),
-    all = JSON.parse(fs.readFileSync(file, 'utf8'));
-  console.info('There are %d objects in %s:', all.length, rel(file));
-  all.forEach((obj) => {
-    console.info('  %s', obj.id);
-  });
+  const file = path.resolve(opts.target, 'eo-foreign.json');
+  try {
+    let content = fs.readFileSync(file, "utf8");
+    content = content.replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+    const all = JSON.parse(content);
+    console.info('There are %d objects in %s:', all.length, rel(file));
+    all.forEach((obj) => {
+      console.info('  %s', obj.id);
+    });
+  } catch (error) {
+    throw new Error(`Parsing a document fails:`, error);
+  }
 };
