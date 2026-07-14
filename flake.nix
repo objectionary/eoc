255e2846323b2fd08e1579bf33cf20cc6be03843
devil <europedreadlydevil@gmail.com>
2026-07-14 20:40:45 +0000

    #1053: run eoc from the nix store, no home copy, jdk on wrapper path

--- flake.nix
+++ flake.nix
@@ -12,28 +12,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        srcPatched = pkgs.runCommand "patched-src" {} ''
-          cp -r ${./.} $out
-          substituteInPlace $out/package.json \
-            --replace-quiet "\"node\": \"^25.0.0\"," ""
-        '';
-        lockHash = builtins.hashFile "sha512" ./package-lock.json;
         packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-        eolangBase = pkgs.buildNpmPackage {
+        srcPatched = pkgs.runCommand "eoc-src" {} ''
+          cp -r ${./.} $out
+          chmod -R u+w $out
+          ${pkgs.jq}/bin/jq 'del(.engines)' ${./.}/package.json > $out/package.json
+        '';
+      in {
+        packages.default = pkgs.buildNpmPackage {
           pname = packageJson.name;
           version = packageJson.version;
           src = srcPatched;
-
-          buildInputs = [ pkgs.nodejs ];
-          dontNpmBuild = true;
-
-          installPhase = ''
-            mkdir -p $out/lib
-            cp -r . $out/lib
-            mkdir -p $out/bin
-            ln -s $out/lib/src/eoc.js $out/bin/eoc
-            chmod +x $out/bin/eoc
-          '';
 
           npmDeps = pkgs.importNpmLock {
             npmRoot = srcPatched;
@@ -41,40 +30,26 @@
 
           npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
+          dontNpmBuild = true;
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib
+            cp -r . $out/lib
+            makeWrapper ${pkgs.nodejs}/bin/node $out/bin/eoc \
+              --add-flags "$out/lib/src/eoc.js" \
+              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jdk pkgs.nodejs ]}
+            runHook postInstall
+          '';
+
           meta = with pkgs.lib; {
             description = packageJson.description;
             homepage = packageJson.homepage;
             license = licenses.mit;
-            author = packageJson.author;
+            mainProgram = "eoc";
           };
-        };
-
-        eolangWrapped = pkgs.writeShellScriptBin "eoc" ''
-          set -euo pipefail
-
-          EOC_STORE="${eolangBase}/lib"
-          EOC_STATE="$HOME/.eoc/lib"
-          EOC_HASH="${lockHash}"
-
-          mkdir -p "$EOC_STATE"
-
-          if [ ! -f "$EOC_STATE/.hash" ] || [ "$(cat $EOC_STATE/.hash)" != "$EOC_HASH" ]; then
-            rm -rf "$EOC_STATE/"
-            cp -r "$EOC_STORE/" "$EOC_STATE/"
-            chmod -R 755 "$EOC_STATE/"
-            echo "$EOC_HASH" > "$EOC_STATE/.hash"
-          fi
-
-          exec "${pkgs.nodejs}/bin/node" "$EOC_STATE/src/eoc.js" "$@"
-        '';
-
-      in {
-        packages.default = eolangWrapped;
-        meta = with pkgs.lib; {
-          description = packageJson.description;
-          homepage = packageJson.homepage;
-          license = licenses.mit;
-          author = packageJson.author;
         };
       }
     );
