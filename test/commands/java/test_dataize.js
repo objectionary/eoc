9f2025482f1edf29bfaa4e4cbcafa2ac4e3f1402
Owwleet <135949439+Owwleet@users.noreply.github.com>
2026-07-24 13:57:09 +0300

    Update test_dataize.js

--- test/commands/java/test_dataize.js
+++ test/commands/java/test_dataize.js
@@ -1,15 +1,12 @@
-const dataize = require('../../../src/commands/java/dataize');
-const verifyJavac = require('../../../src/verify_javac');
-
-jest.mock('../../../src/verify_javac');
-
+const dataize = require('../src/commands/java/dataize');
+const verifyJavac = require('../src/verify_javac');
+jest.mock('../src/verify_javac');
 describe('dataize', () => {
   it('calls verifyJavac', () => {
     jest.spyOn(require('child_process'), 'spawn').mockImplementation(() => ({
       on: jest.fn()
     }));
-
-    dataize('foo', [], {target: '.', stack: '64M', heap: '256M'});
+    dataize('foo', [], { target: '.', stack: '64M', heap: '256M' });
     expect(verifyJavac).toHaveBeenCalled();
   });
-});D
+});
