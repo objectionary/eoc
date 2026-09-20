31a1ae03dd23b9d003f7799567d41b1261e4138c
gemshrine <113778529+gemshrine@users.noreply.github.com>
2026-09-20 20:28:57 +0300

    Fix eoc issue #1360

--- src/mvnw.js
+++ src/mvnw.js
@@ -119,6 +119,12 @@ module.exports.mvnw = function(args, tgt, batch) {
         shell: shell(),
       }
     );
+    result.on('error', (error) => {
+      if (tgt !== undefined && args.includes('--quiet') && !batch) {
+        stop();
+      }
+      reject(error);
+    });
     if (tgt !== undefined && args.includes('--quiet')) {
       if (!batch) {
         start();
